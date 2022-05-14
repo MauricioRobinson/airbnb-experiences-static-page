668cc97480fe70575e32976201126a85b951c7bc
Mauricio Robinson <marsjr97@gmail.com>
2022-05-14 13:50:11 -0400

    Creado el Header del sitio

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HeaderHero from "./HeaderHero";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <HeaderHero />
     </div>
   );
 }
---
+++ src/components/HeaderHero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeaderHero() {
+  return (
+    <header className="bg-red-200 w-full h-96">
+      <img
+        src="./images/boris-misevic-qrz4ASlWgPk-unsplash.jpg"
+        alt="Header del sitio web tomada por Boris Misevic"
+        className="w-full h-96 object-cover"
+      />
+    </header>
+  );
+}
+
+export default HeaderHero;
